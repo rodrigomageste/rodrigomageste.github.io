6c936fa7aa1b1ffd7ebb8d81c0c7ba9b2a865d9a
Rodrigo Mageste R. Pereira <rodrigomageste@hotmail.com>
2015-03-18 21:55:07 -0300

    try github force deploy

--- ideias/js/classie.js
+++ ideias/js/classie.js
@@ -1,7 +1,7 @@
 /*!
  * classie - class helper functions
  * from bonzo https://github.com/ded/bonzo
- * 
+ *
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
@@ -10,6 +10,7 @@
 
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
+
 
 ( function( window ) {
 
